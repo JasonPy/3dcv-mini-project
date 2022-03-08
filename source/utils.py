5d2de3117d628425bd200d61b7be3eecf0d8a11b
json-tp <jason.pyanowski@gmail.com>
2022-03-08 16:43:17 +0100

    Added mean shift mode detection

--- source/utils.py
+++ source/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 from numba import njit
 import time
 from typing import Tuple
+from sklearn.cluster import MeanShift
 
 def get_arrays_size_MB (arrays: Tuple[np.array]):
     return sum([arr.nbytes for arr in arrays]) / 1024 / 1024
@@ -43,3 +44,22 @@ def vector_3d_array_variance(arr: np.array):
     size = arr.shape[0]
     mean = vector_3d_array_mean(arr)
     return np.sum(np.abs(arr - mean)) / size
+
+def get_mode(arr: np.array):
+    """
+    Apply mean shift clustering to obtain modes. 
+    Then find most frequent mode.
+
+    Parameters
+    ----------
+    arr: np.array
+        Array of 3D coordinates to be clustered
+
+    Returns
+    ----------
+    mode: float
+        Most frequent coordinate center after clustering
+    """
+    modes = MeanShift(bandwidth=.1).fit(arr)
+    mode = np.bincount(modes.labels_).argmax()
+    return modes.cluster_centers_[mode]
